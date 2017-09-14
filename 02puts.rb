a6b9f6d050d70b9b7eb0ba5e792c906b01d4cd32
Carolina Tomazetti <carolinatomazetti@gmail.com>
2017-09-14 09:08:10 -0300

    exercicio

---
+++ 02puts.rb
@@ -0,0 +1,20 @@
+puts "Carol".class
+
+puts 3.class
+
+puts 3.15.class
+
+puts true.class
+
+puts false.class
+
+puts [true, "carol", 5].class
+
+puts nil.class
+
+puts :symbol.class
+
+puts "symbol"
+
+puts "symbol".object_id == "symbol".object_id
+puts :symbol.object_id == :symbol.object_id
